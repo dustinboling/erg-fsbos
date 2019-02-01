da6e24b72eed45243e04547db332a4113ea2a5c1
Dustin Boling <dustin@dustinboling.com>
2019-02-01 15:06:02 -0800

    adds Laravel Nova and reverts first_name and last_name back to name

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('layouts.app')
 
 @section('title', 'Willamette Valley FSBOs - Contact Us')
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/listings">Search Listings</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
