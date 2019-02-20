36f32c9d49153663dbe08706d3b795ce37a21e86
Dustin Boling <dustin@dustinboling.com>
2019-02-20 11:26:21 -0800

    replaces single image with a carousel

--- resources/views/listings/partials/listingcard.blade.php
+++ resources/views/listings/partials/listingcard.blade.php
@@ -1,11 +1,31 @@
 @isset($listing)
         <div class="col-md-3">
             <div class="card listing-card" style="margin-bottom:30px;">
-                <a href="{{ route('homes.show', $listing->id) }}">
-                    <img src="https://picsum.photos/308/231/?random&{{ rand() }}" class="card-img-top img-fluid" alt="{{ $listing->street_address }}">
-                </a>
+                {{--  CAROUSEL  --}}
+                <div id="carouselListing{{ $listing->id }}" class="carousel slide" data-ride="carousel" data-interval="false">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100 rounded" src="https://picsum.photos/253/190/?random&{{ rand() }}" alt="Home for sale by owner in {{ $listing->city }}">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100 rounded" src="https://picsum.photos/253/190/?random&{{ rand() }}" alt="Home for sale by owner in {{ $listing->city }}">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100 rounded" src="https://picsum.photos/253/190/?random&{{ rand() }}" alt="Home for sale by owner in {{ $listing->city }}">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselListing{{ $listing->id }}" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselListing{{ $listing->id }}" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                {{-- <img src="https://picsum.photos/308/231/?random&{{ rand() }}" class="card-img-top img-fluid" alt="{{ $listing->street_address }}"> --}}
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item location pt-2">
+                    <li class="list-group-item location pt-2 text-truncate">
                         {{ $listing->community }} | {{ $listing->city }}
                     </li>
                     <li class="list-group-item price">
@@ -21,10 +41,8 @@
                     {{-- <li class="list-group-item id py-2">
                         Web ID: <span class="text-muted">ERG-00{{ $listing->id }}</span>
                     </li> --}}
-                    <li class="list-group-item">
-                        <a href="{{ route('homes.show', $listing->id) }}" class="btn btn-details">More Details</a>
-                    </li>
                 </ul>
+                <a href="{{ route('homes.show', $listing->id) }}" class="btn btn-details py-2">More Details</a>
             </div>
 
         </div>
