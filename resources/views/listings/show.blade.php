53a05474fdad2fa28a64bdb1b25a8d0753988822
Dustin Boling <dustin@dustinboling.com>
2019-02-20 10:15:16 -0800

    replaces image gallery with an image carousel

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -12,15 +12,43 @@
    <div class="row listing">
 
        <div class="col-9 listing-media">
-           <div class="row py-3">
-               <div class="col-10">
-                   <img src="https://picsum.photos/680/480/?random&{{ rand() }}" alt="">
-               </div>
-               <div class="col-2">
-                   <img class="mb-2" src="https://picsum.photos/112/?random&{{ rand() }}" alt="">
-                   <img class="mb-2" src="https://picsum.photos/112/?random&{{ rand() }}" alt="">
-                   <img class="mb-2" src="https://picsum.photos/112/?random&{{ rand() }}" alt="">
-                   <img src="https://picsum.photos/112/?random&{{ rand() }}" alt="">
+           <div class="row">
+               <div class="col-12">
+                    {{--  CAROUSEL  --}}
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="https://picsum.photos/825/470/?random&{{ rand() }}" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="https://picsum.photos/825/470/?random&{{ rand() }}" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="https://picsum.photos/825/470/?random&{{ rand() }}" alt="Third slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="https://picsum.photos/825/470/?random&{{ rand() }}" alt="Fourth slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="https://picsum.photos/825/470/?random&{{ rand() }}" alt="Fifth slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
                </div>
            </div>
        </div>
