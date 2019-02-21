cd0977e878f8cc26a5a5314d110cfc7566e99c45
Dustin Boling <dustin@dustinboling.com>
2019-02-21 11:14:27 -0800

    wip contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -15,11 +15,14 @@
                                 <form action="{{ route('contact') }}" method="post">
                                     @csrf
                                     <div class="form-group">
-                                        <input type="email" name="email" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter your email address">
-                                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                        <input class="form-control" type="name" name="name" id="inputName" placeholder="Enter your name">
                                     </div>
                                     <div class="form-group">
-                                        <textarea class="w-100" name="message" id="inputMessage" cols="30" rows="10" placeholder="Type your message"></textarea>
+                                            <input class="form-control" type="email" name="email" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter your email address">
+                                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                        </div>
+                                    <div class="form-group">
+                                        <textarea class="form-control" class="w-100" name="message" id="inputMessage" cols="30" rows="10" placeholder="Type your message"></textarea>
                                     </div>
                                     <div class="text-right">
                                         <button type="submit" class="btn btn-primary">Send</button>
