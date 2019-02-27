9f9efd328f8987d88bb5726b118f3e11c971b1bb
Dustin Boling <dustin@dustinboling.com>
2019-02-27 14:24:42 -0800

    wip nova theme

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="http://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+    <a href="https://willamettevalleyfsbos.com" class="text-primary dim no-underline">Willamette Valley FSBOs</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
+    &copy; {{ date('Y') }} Eugene Realty Group LLC - By Dustin Boling - Questions? Call: (541) 999-1884
     <span class="px-1">&middot;</span>
     v{{ Laravel\Nova\Nova::version() }}
 </p>
