dfd3ec89999c7e47f46cb3fe67f1bf31a4e9fc37
Dustin Boling <dustin@dustinboling.com>
2019-02-27 14:32:23 -0800

    wip

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,9 @@
 <p class="mt-8 text-center text-xs text-80">
     <a href="https://willamettevalleyfsbos.com" class="text-primary dim no-underline">Willamette Valley FSBOs</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} Eugene Realty Group LLC - By Dustin Boling - Questions? Call: (541) 999-1884
+    &copy; {{ date('Y') }} Eugene Realty Group LLC - By Dustin Boling
+    <span class="px-1">&middot;</span>
+    Questions? (541) 999-1884
     <span class="px-1">&middot;</span>
     v{{ Laravel\Nova\Nova::version() }}
 </p>
