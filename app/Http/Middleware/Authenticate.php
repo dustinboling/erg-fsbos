200fd1c390da20cad928875652036e406602769d
Dustin Boling <dustin@dustinboling.com>
2019-04-23 16:19:44 -0700

    changes redirect for users that are not authenticated. Sends them to login page.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('subscribe.index');
+            return route('login');
         }
     }
 }
