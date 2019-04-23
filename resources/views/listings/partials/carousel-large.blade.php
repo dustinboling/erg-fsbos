29b67a5d78b8a24357e4297e22faea24c5fe2133
Dustin Boling <dustin@dustinboling.com>
2019-04-23 16:18:13 -0700

    adds blur to listing images for guest users, only authenticated users can view all images for a listing.

--- resources/views/listings/partials/carousel-large.blade.php
+++ resources/views/listings/partials/carousel-large.blade.php
@@ -1,13 +1,25 @@
 {{-- A large carousel for full-width display --}}
 <div id="carouselListing{{ $listing->id }}" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
+    <div class="carousel-inner rounded shadow-sm">
         @foreach ($images as $image)
         <div class="carousel-item {{ $loop->first ? 'active' : '' }}"  data-interval="8500">
-            <img class="d-block w-100" src="{{ $image->getUrl('slide') }}" alt="{{ $listing->city->name }}, {{ $listing->state }} home for sale by owner">
-            {{-- $image('slide', ['class' => 'd-block w-100 rounded shadow-sm']) --}}
+            {{-- <img class="d-block w-100" src="{{ $image->getUrl('slide') }}" alt="{{ $listing->city->name }}, {{ $listing->state }} home for sale by owner"> --}}
+            {{-- Adds the blurrable class to secondary images --}}
+            @php $blurrable = $loop->first ? '' : 'blurrable'; @endphp
+            {{ $image('slide', [
+                'class' => "d-block w-100 $blurrable",
+                'alt' => "{$listing->city->name}, {$listing->state} home for sale by owner"
+            ]) }}
+
             @if ($loop->first)
                 <div class="carousel-caption d-none d-sm-block px-4" style="right:0;left:0;background-color:rgba(255,255,255,0.8)">
-                    <h4 class="shadow-sm mb-0 font-weight-normal">Text the code <span style="font-family: 'futura-pt-condensed;font-weight:700;">{{ $listing->text_code }}</span> to the number <span style="font-family: 'futura-pt-condensed;font-weight:700;">88000</span> for an instant tour on your phone!</h4>
+                    <h4 class="mb-0 font-weight-normal">Text the code <span style="font-family: 'futura-pt-condensed;font-weight:700;">{{ $listing->text_code }}</span> to the number <span style="font-family: 'futura-pt-condensed;font-weight:700;">88000</span> for an instant tour on your phone!</h4>
+                </div>
+            @elseif (Auth::guest())
+                <div class="carousel-caption d-none d-sm-block px-4" style="right:0;left:0;background-color:rgba(255,255,255,0.8)">
+                    <h4 class="mb-0 font-weight-normal text-dark">
+                        <a href="{{ route('login') }}">Login</a> or <a href="{{ route('register') }}">Register</a> to View All Photos
+                    </h4>
                 </div>
             @endif
         </div>
--- resources/views/listings/partials/carousel-small.blade.php
+++ resources/views/listings/partials/carousel-small.blade.php
@@ -1,12 +1,22 @@
 {{-- A small carousel for grid of cards --}}
 <div id="carouselListing{{ $listing->id }}" class="carousel slide">
-    <div class="carousel-inner">
-        <?php $isActive = 'active' ?>
+    <div class="carousel-inner rounded">
         @foreach ($images as $image)
-        <div class="carousel-item {{ $isActive }}">
-            {{ $image('square', ['class' => 'd-block w-100 rounded']) }}
-        </div>
-        <?php $isActive = '' ?>
+            <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+                {{-- Adds the blurrable class to secondary images --}}
+                @php $blurrable = $loop->first ? '' : 'blurrable'; @endphp
+                {{ $image('square', [
+                    'class' => "d-block w-100 $blurrable",
+                    'alt' => "{$listing->city->name}, {$listing->state} home for sale by owner"
+                ]) }}
+
+                {{-- Show the overlay to guests on secondary images --}}
+                @if (!$loop->first && Auth::guest())
+                    <div class="carousel-caption d-none d-sm-block px-4" style="right:0;left:0;background-color:rgba(255,255,255,0.8)">
+                        <p class="mb-0 font-weight-normal text-dark"><a href="{{ route('login') }}">Login</a> or <a href="{{ route('register') }}">Register</a><br>to View All Photos</p>
+                    </div>
+                @endif
+            </div>
         @endforeach
     </div>
     <a class="carousel-control-prev" href="#carouselListing{{ $listing->id }}" role="button" data-slide="prev">
