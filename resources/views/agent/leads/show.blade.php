4c233d786bb64e663144126088358d7630bd2a7b
Dustin Boling <dustin@dustinboling.com>
2019-06-25 00:34:42 -0700

    wip - improves layout

--- resources/views/agent/leads/show.blade.php
+++ resources/views/agent/leads/show.blade.php
@@ -11,85 +11,82 @@
         </div>
     </div>
     <div class="row">
-        <div class="col py-3">
+        <div class="col-md-6 py-3">
+                <div class="card mb-4 shadow-sm">
+                        <div class="row no-gutters">
+                            <div class="col-lg-6">
+                                <img src="https://www.gravatar.com/avatar/{{ md5( strtolower( trim( $lead->email ) ) ) }}?s=400" class="card-img" alt="{{ $lead->name }}">
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="card-body">
+                                    <table class="table table-borderless mb-0">
+                                        <tbody>
+                                            <tr>
+                                                <th class="p-2" scope="row" colspan="2">
+                                                    <h5 class="card-title mb-0">{{ $lead->name }}</h5>
+                                                </th>
+                                            </tr>
+                                            <tr>
+                                                <th class="p-2" scope="row">Phone</th>
+                                                <td class="p-2">{{ $lead->phone }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th class="p-2" scope="row">Email</th>
+                                                <td class="p-2">{{ $lead->email }}</td>
+                                            </tr>
+                                            @if (!empty($lead->created_at))
+                                            <tr>
+                                                <th class="p-2" scope="row">
+                                                    <small>Registered</small>
+                                                </th>
+                                                <td class="p-2">
+                                                    <small>{{ date_format($lead->created_at, 'M dS, Y \a\t g:i A') }}</small>
+                                                </td>
+                                            </tr>
+                                            @endif
+                                            @if (!empty($lead->updated_at))
+                                            <tr>
+                                                <th class="p-2" scope="row">
+                                                    <small>Updated</small>
+                                                </th>
+                                                <td class="p-2">
+                                                    <small>{{ date_format($lead->updated_at, 'M dS, Y \a\t g:i A') }}</small>
+                                                </td>
+                                            </tr>
+                                            @endif
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        </div>
+        <div class="col-md-6 py-3">
+            {{-- Most Viewed FSBOs --}}
             <div class="card mb-4 shadow-sm">
-                <div class="row no-gutters">
-                    <div class="col-md-3">
-                        <img src="https://www.gravatar.com/avatar/{{ md5( strtolower( trim( $lead->email ) ) ) }}?s=400" class="card-img" alt="{{ $lead->name }}">
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card-body">
-                            <table class="table table-borderless mb-0" style="max-width:300px">
-                                <tbody>
-                                    <tr>
-                                        <th class="p-2" scope="row" colspan="2">
-                                            <h5 class="card-title mb-0">{{ $lead->name }}</h5>
-                                            <small class="text-muted">Assigned Agent: {{ $lead->agent->name }}</small>
-                                        </th>
-                                    </tr>
-                                    <tr>
-                                        <th class="p-2" scope="row">Phone</th>
-                                        <td class="p-2">{{ $lead->phone }}</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="p-2" scope="row">Email</th>
-                                        <td class="p-2">{{ $lead->email }}</td>
-                                    </tr>
-                                    @if (!empty($lead->created_at))
-                                    <tr>
-                                        <th class="p-2" scope="row">
-                                            <small>Registered</small>
-                                        </th>
-                                        <td class="p-2">
-                                            <small>{{ date_format($lead->created_at, 'M dS, Y \a\t g:i A') }}</small>
-                                        </td>
-                                    </tr>
-                                    @endif
-                                    @if (!empty($lead->updated_at))
-                                    <tr>
-                                        <th class="p-2" scope="row">
-                                            <small>Updated</small>
-                                        </th>
-                                        <td class="p-2">
-                                            <small>{{ date_format($lead->updated_at, 'M dS, Y \a\t g:i A') }}</small>
-                                        </td>
-                                    </tr>
-                                    @endif
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    {{-- Most Viewed FSBOs --}}
-                    <div class="col-md-5">
-                        <div class="card-body">
-                            <table class="table table-borderless mb-0">
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <h5 class="card-title mb-0">Most Viewed FSBOs</h5>
-                                        </td>
-                                    </tr>
-                                    @if ($topViews->isEmpty())
-                                    <tr>
-                                        <td>
-                                            This person hasn't viewed any FSBOs.
-                                        </td>
-                                    </tr>
-                                    @else
+                <div class="card-header text-uppercase" style="letter-spacing:0.1em">Most Viewed</div>
+                <div class="card-body p-0">
+                    <table class="table table-striped mb-0">
+                        <tbody>
+                            @if ($topViews->isEmpty())
+                            <tr>
+                                <td colspan="2">
+                                    This person hasn't viewed any FSBOs.
+                                </td>
+                            </tr>
+                            @else
 
-                                    @endif
-                                    @foreach ($topViews as $view)
-                                    <tr>
-                                        <td>
-                                            <span class="badge badge-primary">{{ $view->user_views }} Views</span>
-                                            {{ $view->viewable->street_address }}, {{ $view->viewable->city->name }}, {{ $view->viewable->state }}
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                            @endif
+                            @foreach ($topViews as $view)
+                            <tr>
+                                <td>
+                                    {{ $view->viewable->street_address }}, {{ $view->viewable->city->name }}, {{ $view->viewable->state }}
+                                </td>
+                                <th scope="col" class="text-center"><span class="badge badge-primary">{{ $view->user_views }} Views</span></th>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
